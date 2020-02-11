dd0b849508b5d9d1aefff7120b951fee79ac983f
hanneslantz <hannes.lantz.97@gmail.com>
2020-02-11 20:59:22 +0100

    fixed search by title and year and imdb_nbr

--- lab3.py
+++ lab3.py
@@ -4,25 +4,52 @@ import sqlite3, json
 db = sqlite3.connect('movies.sqlite')
 cursor = db.cursor()
 
+def response(data):
+    return json.dumps(data, indent=4) + "\n"
+
+def get_by_imdb_nbr(imdb_nbr):
+	c = cursor.execute(
+		"""
+		SELECT imdb_nbr, title, p_year
+		FROM   movies
+		WHERE  imdb_nbr = ?
+		""",
+		[imdb_nbr]
+	)
+	return c
+
 @route('/ping')
 def ping():
 	response.status = 200
 	return "Pong"
 
-@route('/movies', method = 'GET')
-@route('movies/')
-def get_movies():
-	title = request.query.['name']
-	year = request.query.['year']
-	with db: 
-		if len(title) != 0:
-			cursor.execute("SELECT * FROM movies WHERE tile = % AND year = %" (title, year))
-		else: 
-			cursor.execute("SELECT imdb_nbr, title, p_year FROM movies")
-		s = [{"imdbKey": imdb_nbr, "title": title, "year": p_year}
-			for (imdb_nbr, title, p_year) in cursor]
-		return json.dumps({"data": s}, indent=3)
 
+@get('/movies')
+def get_movies():
+	query =	"""
+		SELECT imdb_nbr, title, p_year
+		FROM   movies
+		WHERE  1=1
+		"""
+	p = []
+	if request.query.title:
+		query += "AND title = ?"
+		p.append(request.query.title)
+	if request.query.p_year:
+		query += "AND p_year = ?"
+		p.append(request.query.p_year)
+	cursor.execute(query, p)
+	s = [{"imdb_nbr": imdb_nbr, "title": title, "year": p_year}
+		for (imdb_nbr, title, p_year) in cursor]
+	return response({"data": s})
+
+
+@get('/movies/<imdb_nbr>')
+def get_movie(imdb_nbr):
+	c = get_by_imdb_nbr(imdb_nbr)
+	s = [{"imdb_nbr": imdb_nbr, "title": title, "year": p_year}
+		for (imdb_nbr, title, p_year) in cursor]
+	return response({"data": s})
 
 @route('/reset')
 def reset():
